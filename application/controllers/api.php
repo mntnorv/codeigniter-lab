b8b1ca15848014a358896c9654ca9777f267d9be
Mantas Norvaiša <mntnorv@gmail.com>
2014-02-14 22:52:15 +0200

    Added food to API controller

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -1,17 +1,32 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Home extends Base_Controller {
+class API extends Base_Controller {
 
 	function __construct() {
+		parent::__construct();
 		$this->load->database();
 		$this->load->model('food_model');
+		$this->load->model('food_type_model');
 	}
 
-	public function index() {
-		$all_food = $this->food_model->find_all();
+	public function food($type = "") {
+		$output = [];
+
+		if ($type == "") {
+			$output = $this->food_model->find_all();
+		} else {
+			$types = $this->food_type_model->find_by_name($type);
+
+			if (count($types) != 0) {
+				$type_id = $types[0]->id;
+				$output = $this->food_model->find_by_type($type_id);
+			} else {
+				$output['error'] = 'no such food type';
+			}
+		}
 
 		$this->output
 			->set_content_type('application/json')
-			->set_output(json_encode($all_food));
+			->set_output(json_encode($output));
 	}
 }
