326ea391b2d897e0957b35cc7d5de9683ad08f4f
Mantas Norvaiša <mntnorv@gmail.com>
2014-02-14 22:52:32 +0200

    Seeding food types and food

--- application/controllers/migrate.php
+++ application/controllers/migrate.php
@@ -9,11 +9,45 @@ class Migrate extends CI_Controller {
 			or exit("Execute via command line: php index.php migrate");
 
 		$this->load->library('migration');
+
+		$this->load->model('food_model');
+		$this->load->model('food_type_model');
 	}
 
 	public function index() {
 		if(!$this->migration->latest()) {
 			show_error($this->migration->error_string());
 		}
+	}
+
+	public function seed() {
+		// Seed food types
+		$this->food_type_model->insert(array(
+			"name" => "pizza",
+			"display_name" => "Pica"
+		));
+
+		$this->food_type_model->insert(array(
+			"name" => "drinks",
+			"display_name" => "Gėrimai"
+		));
+
+		$this->food_type_model->insert(array(
+			"name" => "snacks",
+			"display_name" => "Užkandžiai"
+		));
+
+		// Seed food
+		$this->food_model->insert(array(
+			"name" => "Margarita",
+			"type" => "1",
+			"price" => "10"
+		));
+
+		$this->food_model->insert(array(
+			"name" => "Capricioza",
+			"type" => "1",
+			"price" => "13"
+		));
 	}
 }
