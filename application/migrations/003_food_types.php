9671072b8f79f95108a12850886faacaf9e6b31a
Mantas Norvaiša <mntnorv@gmail.com>
2014-02-14 22:43:54 +0200

    Updated food types table

--- application/migrations/003_food_types.php
+++ application/migrations/003_food_types.php
@@ -1,16 +1,18 @@
 <?php
 
 class Migration_Food_Types extends CI_Migration {
-	public function up(){
+	public function up() {
 		$this->dbforge->add_field("id int(11) unsigned NOT NULL AUTO_INCREMENT");
-		$this->dbforge->add_field("name varchar(255) NOT NULL");
+		$this->dbforge->add_field("name varchar(64) NOT NULL");
+		$this->dbforge->add_field("display_name varchar(255) NOT NULL");
 
 		$this->dbforge->add_key('id', TRUE);
+		$this->dbforge->add_key('name');
 	
 		$this->dbforge->create_table('food_types', TRUE);
 	}
 
-	public function down(){
+	public function down() {
 		$this->dbforge->drop_table('food_types');
 	}
 }
