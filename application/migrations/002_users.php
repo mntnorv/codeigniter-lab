5017c172a41eda120c9c8808fcd33ee4b4b861dc
Mantas Norvaiša <mntnorv@gmail.com>
2014-02-15 11:18:08 +0200

    Updated password field length

--- application/migrations/002_users.php
+++ application/migrations/002_users.php
@@ -1,10 +1,10 @@
 <?php
 
 class Migration_Users extends CI_Migration {
-	public function up(){
+	public function up() {
 		$this->dbforge->add_field("id int(11) unsigned NOT NULL AUTO_INCREMENT");
 		$this->dbforge->add_field("username varchar(255) NOT NULL");
-		$this->dbforge->add_field("password varchar(64) NOT NULL");
+		$this->dbforge->add_field("password varchar(255) NOT NULL");
 		$this->dbforge->add_field("user_type int(11) unsigned NOT NULL");
 
 		$this->dbforge->add_key('id', TRUE);
@@ -12,7 +12,7 @@ class Migration_Users extends CI_Migration {
 		$this->dbforge->create_table('users', TRUE);
 	}
 
-	public function down(){
+	public function down() {
 		$this->dbforge->drop_table('users');
 	}
 }
